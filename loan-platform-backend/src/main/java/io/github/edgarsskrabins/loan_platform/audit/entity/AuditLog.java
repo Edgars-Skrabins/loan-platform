aa59a61d360ae2b6aac25d3ea97967693009e99f
Edgars Skrabiņš <shiftkiro@gmail.com>
2026-07-17 15:57:24 +0300

    AuditLog entity

---
+++ loan-platform-backend/src/main/java/io/github/edgarsskrabins/loan_platform/audit/entity/AuditLog.java
@@ -0,0 +1,33 @@
+package io.github.edgarsskrabins.loan_platform.audit.entity;
+
+import io.github.edgarsskrabins.loan_platform.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "audit_logs")
+@Getter
+@Setter
+@NoArgsConstructor
+public class AuditLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String action;
+
+    @CreationTimestamp
+    @Column(name = "timestamp", nullable = false,updatable = false)
+    private Instant timestamp;
+}
